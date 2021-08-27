946cfd928b6537697f4e29b6fdb374ba41de1ac1
Jeffry Gonzalez <jeff_gonzalez@progressive.com>
2021-08-27 12:01:53 -0400

    basic GC with the Thingy

--- MemoryStuff/Program.cs
+++ MemoryStuff/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 
@@ -9,23 +11,38 @@ namespace MemoryStuff
         static void Main(string[] args)
         {
 
-            var x = "Fido";
-            var y = x;
-
-            y = "Tacos";
-
-            Console.WriteLine(x);
-
-            Console.WriteLine(ChangeTheDog(x));
-
-            Console.WriteLine(x);
-
-            string ChangeTheDog(string p)
+            var sw = new Stopwatch();
+            var listOfThings = new List<Thingy>();
+            sw.Start();
+            foreach(var t in Enumerable.Range(1, 100000))
             {
-                p = "Rover";
-                return p.ToUpper();
+                listOfThings.Add(new Thingy(t));
             }
+            sw.Stop();
+            Console.WriteLine($"All Done! - that took {sw.ElapsedMilliseconds} Milliseconds!");
 
+            Console.Write("Hit Enter to Take Out The Trash");
+            Console.ReadLine();
+            listOfThings.Clear();
+            GC.Collect();
+            Console.WriteLine("All Done.");
+
+           
+        }
+    }
+
+    public class Thingy
+    {
+        private readonly int id;
+
+        public Thingy(int id)
+        {
+            this.id = id;
+        }
+
+        ~Thingy()
+        {
+            Console.WriteLine($"Thingy {id}'s Destructor is Running");
         }
     }
 }
